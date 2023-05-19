0ee0e0b3d719bfdc42334ce3564076c28984be65
Jhonatan <jandres.otalvarop@gmail.com>
2023-05-19 18:23:46 -0500

    Código Final

--- main.js
+++ main.js
@@ -43,3 +43,207 @@ const closeNav = () => {
 }
 
 closeBtn.addEventListener('click', closeNav)
+
+/* ACTIVIDAD 1 NIVEL INTERMEDIO */
+
+function verificarPalabra(campoIndex, palabraCorrecta) {
+    const campo = document.getElementById(`campo${campoIndex + 1}`);
+    const valorCampo = campo.value.trim();
+    
+    if (valorCampo.toLowerCase() === palabraCorrecta.toLowerCase()) {
+      campo.style.backgroundColor = 'green';
+      campo.disabled = true;
+      campo.value = palabraCorrecta;
+      campo.setAttribute('title', 'Palabra correcta');
+    } else {
+      campo.style.backgroundColor = '#439A86';
+      campo.removeAttribute('title');
+    }
+  }
+  
+  /* Actividad de Completar Campos */
+
+  function verificarRespuestas() {
+    var respuestasCorrectas = ["Hacer Mientras", "Mientras", "para"];
+    var campo1 = document.getElementById("campo1").value;
+    var campo2 = document.getElementById("campo2").value;
+    var campo3 = document.getElementById("campo3").value;
+    var messageElement = document.getElementById("message");
+  
+    if (campo1.toLowerCase() === respuestasCorrectas[0].toLowerCase() && campo2.toLowerCase() === respuestasCorrectas[1].toLowerCase() && campo3.toLowerCase() === respuestasCorrectas[2].toLowerCase()) {
+      messageElement.textContent = "¡Respuestas correctas!";
+      messageElement.className = "correct";
+    } else {
+      messageElement.textContent = "Respuestas incorrectas. Intenta nuevamente.";
+      messageElement.className = "incorrect";
+    }
+  
+    messageElement.style.display = "block";
+  }
+
+  /* mantenimiento */
+  // Define the term-definition pairs
+const pairs = [
+    {
+      term: 'Análisis del Problema',
+      definition: 'Identificar las funcionalidades que deben incorporarse en el sistema para solucionar el problema'
+    },
+    {
+      term: 'Codificación',
+      definition: 'Transcribir las instrucciones del pseudocódigo a algún lenguaje de programación'
+    },
+    {
+      term: 'Documentación',
+      definition: 'Redactar un documento indicando cómo funciona internamente el programa incluyendo el significado del nombre de las variables'
+    },
+    {
+      term: 'Definición del Problema',
+      definition: 'Explicar de forma clara en qué consiste el problema a resolver'
+    },
+    {
+      term: 'Pruebas y Depuración',
+      definition: 'Verificar el funcionamiento del programa mediante la aplicación de pruebas de sintaxis y lógica'
+    },
+    {
+      term: 'Formulación del Algoritmo',
+      definition: 'Diseñar los algoritmos de las funcionalidades definidas en la etapa de análisis del problema'
+    },
+    {
+      term: 'Mantenimiento',
+      definition: 'Tener en cuenta las sugerencias, ajustes o extensiones que el usuario final requiera para mejoras en el Sistema'
+    }
+  ];
+  
+  // Shuffle the pairs
+  const shuffledPairs = pairs.sort(() => Math.random() - 0.5);
+  
+  // Display the term-definition pairs on the cards
+  const cards = document.querySelectorAll('.card');
+  cards.forEach((card, index) => {
+    const term = card.querySelector('h2');
+    term.textContent = shuffledPairs[index].term;
+    card.querySelector('p.definition').textContent = shuffledPairs[index].definition;
+  });
+  
+  let selectedCard = null;
+  let matchedPairs = 0;
+  
+  cards.forEach((card) => {
+    card.addEventListener('click', () => {
+      if (card.classList.contains('selected')) {
+        return;
+      }
+  
+      card.classList.add('selected');
+  
+      if (selectedCard === null) {
+        selectedCard = card;
+      } else {
+        const firstTerm = selectedCard.querySelector('h2').textContent;
+        const secondTerm = card.querySelector('h2').textContent;
+  
+        if (firstTerm === secondTerm) {
+          selectedCard.removeEventListener('click', () => {});
+          card.removeEventListener('click', () => {});
+          selectedCard = null;
+          matchedPairs++;
+  
+          if (matchedPairs === pairs.length / 2) {
+            setTimeout(() => {
+              alert('¡Has completado todas las definiciones!');
+            }, 500);
+          }
+        } else {
+          setTimeout(() => {
+            selectedCard.classList.remove('selected');
+            card.classList.remove('selected');
+            selectedCard = null;
+          }, 500);
+        }
+      }
+    });
+  });
+  
+  /* mantenimiento */
+
+
+  /* Modulo4-pseudocodigo */
+  function checkVisibility() {
+    const paragraphs = document.querySelectorAll('.caracteristicasDisenio p');
+    
+    paragraphs.forEach((paragraph) => {
+      const position = paragraph.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+  
+      if (position < windowHeight) {
+        paragraph.classList.add('visible');
+      }
+    });
+  }
+  
+  window.addEventListener('scroll', checkVisibility);
+  
+  /* Modulo4-pseudocodigo */
+
+  /* actividadForm */
+  document.getElementById('actividadForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita que se recargue la página al enviar el formulario
+    
+    // Obtiene los valores ingresados por el usuario
+    const instrucciones = document.getElementById('instrucciones').value.trim().toLowerCase();
+    const variable = document.getElementById('variable').value.trim().toLowerCase();
+    const condicionales = document.getElementById('condicionales').value.trim().toLowerCase();
+    const iterativas = document.getElementById('iterativas').value.trim().toLowerCase();
+    const secuenciales = document.getElementById('secuenciales').value.trim().toLowerCase();
+    const programa = document.getElementById('programa').value.trim().toLowerCase();
+    
+    // Define las respuestas correctas
+    const respuestasCorrectas = {
+        instrucciones: 'en programación, las instrucciones son comandos o líneas de código que indican a un programa qué acciones debe realizar',
+        variable: 'una variable es un contenedor que almacena un valor en la memoria durante la ejecución de un programa',
+        condicionales: 'las condicionales son estructuras de control utilizadas en programación para tomar decisiones basadas en una condición',
+        iterativas: 'las estructuras iterativas, también conocidas como bucles, permiten repetir un conjunto de instrucciones varias veces',
+        secuenciales: 'las instrucciones secuenciales son aquellas que se ejecutan en orden, una tras otra, en un programa',
+        programa: 'un programa es un conjunto de instrucciones escritas en un lenguaje de programación que se utiliza para realizar una tarea específica en una computadora'
+    };
+    
+    // Verifica las respuestas y muestra el resultado
+    const resultadoDiv = document.getElementById('resultado');
+    resultadoDiv.innerHTML = '';
+
+    for (const key in respuestasCorrectas) {
+        const respuestaUsuario = eval(key);
+        const respuestaCorrecta = respuestasCorrectas[key];
+        
+        const resultadoItem = document.createElement('p');
+        resultadoItem.textContent = key.charAt(0).toUpperCase() + key.slice(1) + ': ';
+        
+        if (respuestaUsuario === respuestaCorrecta) {
+            resultadoItem.textContent += 'Correcto';
+            resultadoItem.style.color = 'green';
+        } else {
+            resultadoItem.textContent += 'Incorrecto';
+            resultadoItem.style.color = 'red';
+        }
+        
+        resultadoDiv.appendChild(resultadoItem);
+    }
+});
+
+  /* actividadForm */
+
+  function goToNextSection() {
+    const selectedRating = document.querySelector('input[name="rating"]:checked');
+    if (selectedRating) {
+        // Bloquear las estrellas
+        const stars = document.querySelectorAll('.rating input');
+        for (let i = 0; i < stars.length; i++) {
+            stars[i].disabled = true;
+        }
+
+        // Navegar a la siguiente sección
+        window.location.href = "siguiente_seccion.html";
+    } else {
+        alert("Por favor, seleccione una valoración.");
+    }
+}
--- pregunta1.js
+++ pregunta1.js
@@ -1,45 +1,366 @@
-/*   const formulario = document.getElementById('formulario');
-  const seccionOculta = document.getElementById('seccion-oculta');
+//Constantes Primera Pregunta
+const formulario = document.getElementById('formulario');
+const seccionOculta = document.getElementById('seccion-oculta');
+const botonCierreOculto = document.getElementById('boton-cierre');
 
-  formulario.addEventListener('submit', function(evento) {
-    evento.preventDefault();
-    const respuesta = document.getElementById('respuesta').value;
-    if (respuesta === 'Lógica es la técnica utilizada para desarrollar instrucciones en una secuencia para lograr determinado objetivo.​') {
-      seccionOculta.style.display = 'block';
-      formulario.classList.add('oculto');
-    } else {
-      alert('Respuesta incorrecta');
-    } 
-    botonCierreOculto.addEventListener('click', function() {
-        seccionOculta.style.display = 'none';
-        botonCierre.style.display = 'block';
-        botonCierreOculto.style.display = 'none';
-        formulario.reset();
-        formulario.classList.remove('oculto');
-      });
-  });
-
+// Primera Pregunta.
   
- */
+formulario.addEventListener('submit', function(evento) {
+  evento.preventDefault();
+  const respuesta = document.getElementById('respuesta').value;
+  if (respuesta === 'Lógica es la técnica utilizada para desarrollar instrucciones en una secuencia para lograr determinado objetivo.​') {
+    seccionOculta.style.display = 'block';
+    formulario.style.display = 'none';
+    const audioCorrecto = document.getElementById('audio-correcto');
+    audioCorrecto.play();
+  } else {
+    alert('Respuesta incorrecta. Por favor, inténtalo de nuevo.');
+  }
+});
 
-  const formulario = document.getElementById('formulario');
-  const seccionOculta = document.getElementById('seccion-oculta');
-  const botonCierreOculto = document.getElementById('boton-cierre');
-  
-  formulario.addEventListener('submit', function(evento) {
-    evento.preventDefault();
-    const respuesta = document.getElementById('respuesta').value;
-    if (respuesta === 'Lógica es la técnica utilizada para desarrollar instrucciones en una secuencia para lograr determinado objetivo.​') {
-      seccionOculta.style.display = 'block';
-      formulario.style.display = 'none';
-    } else {
-      alert('Respuesta incorrecta');
+botonCierreOculto.addEventListener('click', function() {
+  seccionOculta.style.display = 'none';
+  formulario.style.display = 'block';
+  formulario.reset();
+});
+
+// Fin primera pregunta
+
+// Inicio de la actividad Unir
+const terminos = document.querySelectorAll('.termino');
+const definiciones = document.querySelectorAll('.definicion');
+let terminoSeleccionado = null;
+let definicionSeleccionada = null;
+let actividadTerminada = false;
+let terminosCorrectos = 0;
+
+terminos.forEach((termino) => {
+  termino.addEventListener('click', () => {
+    if (actividadTerminada || termino.classList.contains('bloqueado')) {
+      return;
+    }
+
+    if (terminoSeleccionado) {
+      terminoSeleccionado.classList.remove('seleccionado');
+    }
+
+    terminoSeleccionado = termino;
+    terminoSeleccionado.classList.add('seleccionado');
+
+    if (definicionSeleccionada) {
+      definicionSeleccionada.classList.remove('seleccionado');
+      definicionSeleccionada = null;
     }
   });
-  
-  botonCierreOculto.addEventListener('click', function() {
-    seccionOculta.style.display = 'none';
-    formulario.style.display = 'block';
-    formulario.reset();
+});
+
+definiciones.forEach((definicion) => {
+  definicion.addEventListener('click', () => {
+    if (actividadTerminada || definicion.classList.contains('bloqueado')) {
+      return;
+    }
+
+    if (terminoSeleccionado) {
+      definicionSeleccionada = definicion;
+      definicionSeleccionada.classList.add('seleccionado');
+
+      if (verificarCorrespondencia(terminoSeleccionado.getAttribute('data-definicion'), definicionSeleccionada.getAttribute('id'))) {
+        terminosCorrectos++;
+        terminoSeleccionado.classList.add('bloqueado');
+        definicionSeleccionada.classList.add('bloqueado');
+        terminoSeleccionado = null;
+        definicionSeleccionada = null;
+
+        if (terminosCorrectos === terminos.length) {
+          actividadTerminada = true;
+          mostrarMensajeActividadCompletada();
+        } else {
+          mostrarIconoCorrecto(definicionSeleccionada);
+        }
+      } else {
+        terminoSeleccionado.classList.remove('seleccionado');
+        definicionSeleccionada.classList.remove('seleccionado');
+        terminoSeleccionado = null;
+        definicionSeleccionada = null;
+        alert('Respuesta incorrecta. Inténtalo de nuevo.');
+      }
+    }
   });
+});
+
+function verificarCorrespondencia(termino, definicion) {
+  return termino === definicion;
+}
+
+function mostrarIconoCorrecto(definicion) {
+  const icono = document.createElement('span');
+  icono.classList.add('icono-correcto');
+  definicion.appendChild(icono);
+}
+
+function mostrarMensajeActividadCompletada() {
+  const mensaje = document.createElement('p');
+  mensaje.textContent = 'Actividad completada';
+  const actividad = document.querySelector('.actividad');
+  actividad.appendChild(mensaje);
+}
+
+function mostrarAlerta(mensaje) {
+  const alerta = document.createElement('div');
+  alerta.classList.add('alerta');
+  alerta.textContent = mensaje;
+  const actividad = document.querySelector('.actividad');
+  actividad.insertBefore(alerta, actividad.firstChild);
+  setTimeout(() => {
+    alerta.remove();
+  }, 3000);
+}
+
+// SOPA DE LETRAS
+
+// Palabras a encontrar
+const words = ['Deductivo', 'Analítico', 'Abstracción', 'Detección'];
+
+// Sopa de letras
+const puzzle = [
+    ['D', 'E', 'T', 'E', 'C', 'C', 'I', 'O', 'N', 'Z', 'X', 'C', 'P', 'Z', 'N', 'A'],
+    ['E', 'E', 'Y', 'M', 'V', 'R', 'P', 'A', 'Q', 'Q', 'B', 'W', 'Z', 'X', 'W', 'E'],
+    ['R', 'X', 'D', 'A', 'A', 'E', 'O', 'C', 'W', 'A', 'I', 'Q', 'C', 'S', 'I', 'N'],
+    ['T', 'C', 'H', 'U', 'L', 'R', 'R', 'Y', 'E', 'S', 'R', 'E', 'X', 'D', 'N', 'Y'],
+    ['R', 'V', 'J', 'A', 'C', 'M', 'C', 'C', 'R', 'Z', 'D', 'C', 'R', 'E', 'X', 'A'],
+    ['A', 'Q', 'L', 'B', 'A', 'T', 'Y', 'A', 'T', 'X', 'H', 'T', 'E', 'Q', 'N', 'N'],
+    ['S', 'W', 'S', 'S', 'I', 'T', 'I', 'W', 'Y', 'C', 'U', 'R', 'Q', 'E', 'I', 'N'],
+    ['Z', 'E', 'Z', 'T', 'A', 'D', 'N', 'V', 'U', 'V', 'T', 'S', 'Q', 'Y', 'N', 'Y'],
+    ['X', 'Y', 'K', 'R', 'S', 'E', 'M', 'R', 'O', 'F', 'A', 'E', 'X', 'T', 'N', 'N'],
+    ['C', 'U', 'I', 'A', 'N', 'A', 'L', 'I', 'T', 'I', 'C', 'O', 'Z', 'R', 'X', 'A'],
+    ['V', 'A', 'E', 'C', 'I', 'C', 'H', 'I', 'P', 'G', 'O', 'D', 'S', 'L', 'N', 'N'],
+    ['B', 'S', 'U', 'C', 'C', 'L', 'O', 'L', 'A', 'V', 'U', 'F', 'A', 'D', 'N', 'N'],
+    ['B', 'Z', 'S', 'I', 'L', 'V', 'G', 'O', 'S', 'H', 'R', 'A', 'W', 'N', 'I', 'Y'],
+    ['V', 'X', 'A', 'O', 'A', 'T', 'K', 'C', 'D', 'J', 'E', 'S', 'I', 'A', 'X', 'N'],
+    ['A', 'C', 'S', 'N', 'V', 'E', 'I', 'I', 'F', 'Y', 'R', 'A', 'A', 'N', 'A', 'A'],
+    ['B', 'C', 'R', 'Y', 'M', 'W', 'W', 'Q', 'G', 'R', 'T', 'Z', 'N', 'A', 'X', 'S'],
+    ['A', 'N', 'Q', 'R', 'R', 'Q', 'A', 'A', 'H', 'T', 'T', 'C', 'I', 'N', 'I', 'A'],
+    ['N', 'L', 'R', 'E', 'C', 'A', 'R', 'Q', 'J', 'I', 'Q', 'X', 'A', 'I', 'N', 'N'],
+    ['C', 'Z', 'A', 'Q', 'L', 'Z', 'T', 'R', 'K', 'U', 'W', 'C', 'X', 'N', 'N', 'X'],
+    ['A', 'E', 'N', 'K', 'A', 'X', 'S', 'T', 'L', 'H', 'E', 'B', 'N', 'Y', 'X', 'I'],
+    ['W', 'Z', 'Q', 'J', 'F', 'C', 'L', 'Y', 'Ñ', 'J', 'T', 'V', 'Y', 'N', 'I', 'N'],
+    ['E', 'G', 'A', 'K', 'E', 'V', 'E', 'U', 'Z', 'P', 'Y', 'N', 'N', 'Y', 'N', 'O'],
+    ['P', 'G', 'G', 'Y', 'R', 'U', 'G', 'I', 'X', 'E', 'X', 'M', 'A', 'A', 'Y', 'Y'],
+];
+
+// Generar la sopa de letras en el HTML
+function generatePuzzle() {
+    const puzzleDiv = document.getElementById('puzzle');
+    const table = document.createElement('table');
+
+    for (let i = 0; i < puzzle.length; i++) {
+        const row = document.createElement('tr');
+        for (let j = 0; j < puzzle[i].length; j++) {
+            const cell = document.createElement('td');
+            cell.textContent = puzzle[i][j];
+            cell.addEventListener('click', () => selectCell(cell));
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    }
+
+    puzzleDiv.appendChild(table);
+}
+
+// Encontrar las palabras en la sopa de letras
+function findWords() {
+    const puzzleDiv = document.getElementById('puzzle');
+    const table = puzzleDiv.getElementsByTagName('table')[0];
+
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+        const cells = table.getElementsByTagName('td');
+
+        for (let j = 0; j < cells.length; j++) {
+            const cell = cells[j];
+            if (cell.textContent === word[0]) {
+                const found = checkWord(cell, word);
+                if (found) break;
+            }
+        }
+    }
+}
+
+// Verificar si una palabra se encuentra en una dirección determinada
+function checkWord(cell, word) {
+    const directions = [
+        [0, 1],   // Derecha
+        [1, 0],   // Abajo
+        [1, 1],   // Diagonal hacia abajo-derecha
+        [-1, 1]  // Diagonal hacia arriba-derecha
+    ];
+
+    for (let i = 0; i < directions.length; i++) {
+        const direction = directions[i];
+        const row = cell.parentNode.rowIndex;
+        const col = cell.cellIndex;
+        let found = true;
+
+        if (row + (direction[0] * word.length) < 0 || row + (direction[0] * word.length) >= puzzle.length) {
+            continue;
+        }
+
+        if (col + (direction[1] * word.length) < 0 || col + (direction[1] * word.length) >= puzzle[row].length) {
+            continue;
+        }
+
+        for (let j = 1; j < word.length; j++) {
+            const newRow = row + (direction[0] * j);
+            const newCol = col + (direction[1] * j);
+            const cell = table.rows[newRow].cells[newCol];
+
+            if (cell.textContent !== word[j]) {
+                found = false;
+                break;
+            }
+        }
+
+        if (found) {
+            markWord(cell, word.length, direction);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// Marcar la palabra encontrada en la sopa de letras
+function markWord(cell, length, direction) {
+    let row = cell.parentNode.rowIndex;
+    let col = cell.cellIndex;
+
+    for (let i = 0; i < length; i++) {
+        cell.style.backgroundColor = 'yellow';
+        cell = table.rows[row + (direction[0] * i)].cells[col + (direction[1] * i)];
+    }
+
+    const wordSpan = document.createElement('span');
+    wordSpan.className = 'word';
+    wordSpan.textContent = cell.innerHTML;
+    puzzleDiv.appendChild(wordSpan);
+}
+
+// Seleccionar una celda
+function selectCell(cell) {
+    const selectedCells = document.getElementsByClassName('selected');
+    if (selectedCells.length > 0) {
+        // Desmarcar la celda previamente seleccionada
+        selectedCells[0].classList.remove('selected');
+    }
+
+    // Marcar la nueva celda seleccionada
+    cell.classList.add('selected');
+
+    const selectedWord = getSelectedWord();
+    if (selectedWord) {
+        const found = checkWord(selectedWord.startCell, selectedWord.word);
+        if (found) {
+            const selectedCells = document.getElementsByClassName('selected');
+            while (selectedCells.length > 0) {
+                selectedCells[0].classList.remove('selected');
+            }
+        }
+    }
+}
+
+// Obtener la palabra seleccionada hasta la celda actual
+function getSelectedWord() {
+    const selectedCells = document.getElementsByClassName('selected');
+    if (selectedCells.length > 0) {
+        const startCell = selectedCells[0];
+        let word = '';
+        let currentCell = startCell;
+        while (currentCell) {
+            word += currentCell.textContent;
+            if (currentCell.classList.contains('selected')) {
+                currentCell = getNextCell(currentCell);
+            } else {
+                break;
+            }
+        }
+        return {
+            startCell: startCell,
+            word: word
+        };
+    }
+    return null;
+}
+
+// Obtener la siguiente celda adyacente a la actual
+function getNextCell(cell) {
+    const row = cell.parentNode.rowIndex;
+    const col = cell.cellIndex;
+
+    const directions = [
+        [0, 1],   // Derecha
+        [1, 0],   // Abajo
+        [1, 1],   // Diagonal hacia abajo-derecha
+        [-1, 1]  // Diagonal hacia arriba-derecha
+    ];
+
+    const selectedDirection = getSelectedDirection();
+
+    for (let i = 0; i < directions.length; i++) {
+        const direction = directions[i];
+        if (
+            direction[0] === selectedDirection[0] &&
+            direction[1] === selectedDirection[1]
+        ) {
+            const newRow = row + direction[0];
+            const newCol = col + direction[1];
+            if (
+                newRow >= 0 &&
+                newRow < puzzle.length &&
+                newCol >= 0 &&
+                newCol < puzzle[newRow].length
+            ) {
+                return table.rows[newRow].cells[newCol];
+            }
+        }
+    }
+
+    return null;
+}
+
+// Obtener la dirección seleccionada en base a las celdas seleccionadas
+function getSelectedDirection() {
+    const selectedCells = document.getElementsByClassName('selected');
+    if (selectedCells.length > 1) {
+        const currentCell = selectedCells[selectedCells.length - 1];
+        const previousCell = selectedCells[selectedCells.length - 2];
+
+        const rowDiff = currentCell.parentNode.rowIndex - previousCell.parentNode.rowIndex;
+        const colDiff = currentCell.cellIndex - previousCell.cellIndex;
+
+        return [rowDiff, colDiff];
+    }
+
+    return [0, 0];
+}
+
+// Generar la sopa de letras y encontrar las palabras
+generatePuzzle();
+findWords();
+setTimeout(findWords, 1000);
+
+// Completar campos
+
+function validarCampos() {
+  var input1 = document.getElementById("input1");
+  var input2 = document.getElementById("input2");
+  var input3 = document.getElementById("input3");
+  var input4 = document.getElementById("input4");
+  var input5 = document.getElementById("input5");
+  var input6 = document.getElementById("input6");
   
+  if (input1.value && input2.value && input3.value && input4.value && input5.value && input6.value) {
+    var mensajeCorrecto = document.getElementById("mensaje-correcto");
+    mensajeCorrecto.style.display = "block";
+  }
+}
+
